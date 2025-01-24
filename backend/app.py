58ab17a540b716c2738e1afeeb43e47ac4b07d0b
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2025-01-24 12:27:45 +0300

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,49 +1,35 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
 
-# import logging
-
-
-
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://new_user:your_password@localhost:5432/edulearn'
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 jwt = JWTManager(app)
-
-
-
-
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 CORS(app, origins="http://localhost:3000")
-migrate = Migrate(app, db)  # Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
-
-# Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-# logging.basicConfig(level=logging.DEBUG)
 
-from routes import auth_routes  # Import the routes from routes.py
-# Routes will be added here
+from routes import auth_routes
 from courses import courses_routes
 
-# Register routes
-app.register_blueprint(auth_routes, url_prefix='/auth')  # Register the blueprint with a prefix
-# Register the blueprint with the prefix '/api'
+app.register_blueprint(auth_routes, url_prefix='/auth')
 app.register_blueprint(courses_routes, url_prefix='/api')
+
 if __name__ == "__main__":
     app.run(debug=True)
